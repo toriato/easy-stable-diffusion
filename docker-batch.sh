a029a18fc72c55ed6a147499fb974b0ac6f6ce03
Sangha Lee <totoriato@gmail.com>
2022-12-27 04:32:52 +0900

    add `PIP_TORCH_PACKAGE` env
    Former-commit-id: 4416bd1c67d023688911cc1d0aa0886e6e9cab78

--- docker-batch.sh
+++ docker-batch.sh
@@ -7,7 +7,7 @@ shopt -s nullglob
 [[ "${TRACE-0}" == "1" ]] && set -o xtrace
 
 cuda_versions=(
-  # "cu113"
+  "cu113"
   "cu116"
 )
 
@@ -18,9 +18,9 @@ cuda_images["cu116"]="docker.io/nvidia/cuda:11.6.0-devel-ubuntu18.04"
 # xformers supports 3.7 to 3.10
 # https://github.com/facebookresearch/xformers/blob/e163309908ed7a76847ce46c79b238b49fd7d341/setup.py#L335-L338
 python_versions=(
-  # "3.7"
-  # "3.8"
-  # "3.9"
+  "3.7"
+  "3.8"
+  "3.9"
   "3.10"
 )
 
@@ -34,6 +34,7 @@ for cuda_version in "${cuda_versions[@]}"; do
     TRACE="${TRACE-0}" \
     PYTHON_VERSION="${python_version}" \
     PIP_TORCH_INDEX="https://download.pytorch.org/whl/${cuda_version}" \
+    PIP_TORCH_PACKAGE="torch==1.12.1+${cuda_version}" \
     DOCKER_IMAGE="${cuda_images["$cuda_version"]}" \
       ./docker.sh
     
