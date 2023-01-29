f169140da1d23135f1d93f7458557fda398581b8
Sangha Lee <totoriato@gmail.com>
2023-01-30 06:32:15 +0900

    feat: 작업 경로와 관련된 코드 분리

--- modules/shared.py
+++ modules/shared.py
@@ -1,46 +1,18 @@
-import itertools
-
-from typing import Iterable, List, Callable
 from pathlib import Path
-
-from modules.ui import Selector, SelectorOption
+from datetime import datetime
+from IPython import get_ipython
 
 PYTHON_EXECUTABLE = 'python3.10'
 
-APP_DIR = Path(__file__).parent
+APP_DIR = Path(__file__).parent.parent
+REPO_DIR = APP_DIR.joinpath('repository')
 GDRIVE_MOUNT_DIR = APP_DIR.joinpath('drive')
 GDRIVE_DIR = GDRIVE_MOUNT_DIR.joinpath('MyDrive')
-REPO_DIR = APP_DIR.joinpath('repository')
+WORKSPACE_DIR = APP_DIR.joinpath('workspace')
 
-workspace = Selector(
-    options=[
-        SelectorOption('SD')
-    ]
+IN_COLAB = 'google.colab' in str(get_ipython())
+
+LOG_PATH = APP_DIR.joinpath(
+    'logs',
+    datetime.now().strftime('%Y-%m-%d_%H-%M-%S.log')
 )
-
-
-def workspace_lookup_generator(
-    globs: List[str]
-) -> Callable[..., Iterable[SelectorOption]]:
-    # TODO: 기본 작업 경로는 레포지토리를 가르켜야함
-    workspace_path = workspace.extract() or REPO_DIR
-
-    def func():
-        # glob 패턴을 통해 일치하는 모든 하위 파일 목록 가져오기
-        path_chunks = map(
-            lambda pattern: [p for p in workspace_path.glob(pattern)],
-            globs
-        )
-
-        # 2차원 배열을 1차원 배열로 펼치기
-        options = [
-            SelectorOption(str(path))
-            for path in itertools.chain(*path_chunks)
-        ]
-
-        if options:
-            options.insert(0, SelectorOption('< 자동 선택 >', lambda _: None))
-
-        return options
-
-    return func
---
+++ modules/workspace.py
@@ -0,0 +1,61 @@
+import os
+import itertools
+
+from typing import Iterable, List, Callable
+
+from modules import shared
+from modules.ui import Selector, SelectorOption, SelectorText
+
+
+prepend_options = []
+
+if shared.IN_COLAB:
+    prepend_options.append(
+        SelectorText(
+            '< 구글 드라이브 동기화 >',
+            'SD',
+            lambda option: os.path.join(
+                shared.GDRIVE_DIR,
+                str(option.widget.value)
+            )
+        )
+    )
+
+workspace = Selector((
+    *prepend_options,
+    SelectorText(
+        '< 로컬 파일 시스템 >',
+        'SD',
+        lambda option: os.path.join(
+            shared.APP_DIR,
+            str(option.widget.value)
+        )
+    )
+))
+
+
+def workspace_lookup_generator(
+    globs: List[str]
+) -> Callable[..., Iterable[SelectorOption]]:
+    # TODO: 기본 작업 경로는 레포지토리를 가르켜야함
+    workspace_path = workspace.extract() or shared.REPO_DIR
+
+    def func():
+        # glob 패턴을 통해 일치하는 모든 하위 파일 목록 가져오기
+        path_chunks = map(
+            lambda pattern: [p for p in workspace_path.glob(pattern)],
+            globs
+        )
+
+        # 2차원 배열을 1차원 배열로 펼치기
+        options = [
+            SelectorOption(str(path))
+            for path in itertools.chain(*path_chunks)
+        ]
+
+        if options:
+            options.insert(0, SelectorOption('< 자동 선택 >', lambda _: None))
+
+        return options
+
+    return func
