7cd0d38a355c60662083447dd7cc88088354e0eb
Sangha Lee <totoriato@gmail.com>
2023-01-31 14:29:48 +0900

    fix: minor typo

--- modules/git.py
+++ modules/git.py
@@ -3,7 +3,7 @@ from . import shared, subprocess
 
 if not importlib.util.find_spec('git'):
     if shared.IN_COLAB:
-        subprocess.call('pip', 'install', 'GitPython')
+        subprocess.call(['pip', 'install', 'GitPython'])
     else:
         raise ImportError('GitPython 모듈을 찾을 수 없습니다')
 
