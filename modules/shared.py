488f64aec5b75ec148e89170e39503d276b33ab3
Sangha Lee <totoriato@gmail.com>
2023-01-31 18:47:21 +0900

    feat: 코랩 환경 흉내낼 수 있는 환경변수 추가

--- modules/shared.py
+++ modules/shared.py
@@ -1,14 +1,11 @@
-from pathlib import Path
 from datetime import datetime
+from os import environ
+from pathlib import Path
+
 from IPython import get_ipython
 
 APP_DIR = Path(__file__).parent.parent
 GDRIVE_MOUNT_DIR = APP_DIR.joinpath('drive')
 GDRIVE_DIR = GDRIVE_MOUNT_DIR.joinpath('MyDrive')
 
-IN_COLAB = 'google.colab' in str(get_ipython())
-
-LOG_PATH = APP_DIR.joinpath(
-    'logs',
-    datetime.now().strftime('%Y-%m-%d_%H-%M-%S.log')
-)
+IN_COLAB = 'IN_COLAB' in environ or 'google.colab' in str(get_ipython())
