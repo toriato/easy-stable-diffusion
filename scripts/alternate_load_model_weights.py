4b375c7ba09e86cef612ef3d7c6395aa8c43fadf
Sangha Lee <totoriato@gmail.com>
2023-02-10 04:10:35 +0900

    ...

--- scripts/alternate_load_model_weights.py
+++ scripts/alternate_load_model_weights.py
@@ -49,7 +49,7 @@ def on_app_started(*args, **kwargs):
         # 사용 가능한 메모리가 충분할 때만 모델 불러오기
         if 4 < meminfo['MemAvailable'] / 1024 / 1024:
             return call_queue.wrap_queued_call(
-                sd_models.reload_model_weights
+                lambda: sd_models.reload_model_weights()
             )()
 
         # 클라이언트에게 결과를 반환하지 않으면 설정을 다시 바꿀 수 없게 되어버림
